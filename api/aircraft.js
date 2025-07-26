3194590dbf966208e32d4ede0ba17668127c4bde
Miguel Teixeira <miguelt007@gmail.com>
2025-07-26 13:41:00 +0100

    Novo aircraft.js

--- api/aircraft.js
+++ api/aircraft.js
@@ -1,19 +1,22 @@
 export default async function handler(req, res) {
-  const response = await fetch(
-    "https://aircraftscatter.p.rapidapi.com/lat/38.7223/lon/-9.1393/",
-    {
-      method: "GET",
+  res.setHeader('Access-Control-Allow-Origin', '*'); // <- permite qualquer origem
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  const lat = 38.7223;  // Lisboa
+  const lon = -9.1393;
+
+  try {
+    const response = await fetch(`https://aircraftscatter.p.rapidapi.com/lat/${lat}/lon/${lon}/`, {
       headers: {
-        "x-rapidapi-host": "aircraftscatter.p.rapidapi.com",
-        "x-rapidapi-key": process.env.RAPIDAPI_KEY
+        'x-rapidapi-host': 'aircraftscatter.p.rapidapi.com',
+        'x-rapidapi-key': process.env.RAPIDAPI_KEY,
       }
-    }
-  );
+    });
 
-  if (!response.ok) {
-    return res.status(response.status).json({ error: "Erro ao buscar aeronaves" });
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar aeronaves" });
   }
-
-  const data = await response.json();
-  res.status(200).json(data);
 }

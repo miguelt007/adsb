923c615835937e5433ca3a5b44652fa8ad1c8fff
Miguel Teixeira <miguelt007@gmail.com>
2025-07-26 12:50:37 +0100

    Update aircraft.js

--- api/aircraft.js
+++ api/aircraft.js
@@ -17,3 +17,4 @@ export default async function handler(req, res) {
   const data = await response.json();
   res.status(200).json(data);
 }
+console.log("Chave recebida do Vercel:", process.env.RAPIDAPI_KEY);
